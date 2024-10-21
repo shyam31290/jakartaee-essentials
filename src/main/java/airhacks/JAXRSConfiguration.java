47eab342248b7b91faefa10e513e626c980a8074
Adam Bien <abien@adam-bien.com>
2024-10-21 18:24:20 +0200

    JAXRSConfiguration added

---
+++ src/main/java/airhacks/JAXRSConfiguration.java
@@ -0,0 +1,15 @@
+package airhacks;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures a JAX-RS endpoint. Delete this class, if you are not exposing
+ * JAX-RS resources in your application.
+ *
+ * @author airhacks.com
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+
+}
